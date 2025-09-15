a060dd8cf0711467951b3c7ce9ee156c5e16e1c4
Ben Hacker <Sigabiel123@gmail.com>
2025-09-15 17:20:09 +0200

    remove unused form

--- apps/webapp/src/routes/SignIn.tsx
+++ apps/webapp/src/routes/SignIn.tsx
@@ -1,28 +1,36 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { request } from '@/lib/utils'
-import { toast } from 'sonner'
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { request } from '@/lib/utils';
+import { toast } from 'sonner';
 
 const SignInPage = () => {
     const handleSubmit = async () => {
-        const username = (document.getElementById('username') as HTMLInputElement).value.trim()
-        const password = (document.getElementById('password') as HTMLInputElement).value.trim()
+        const username = (document.getElementById('username') as HTMLInputElement).value.trim();
+        const password = (document.getElementById('password') as HTMLInputElement).value.trim();
 
         // TODO add 2FA
-        if (username.length < 3 || password.length < 3) return toast.warning('Please enter longer credentials.')
-        const res = await request('GET', `/api/authentication/login?username=${username}&password=${password}`)
-        console.log(res)
+        if (username.length < 3 || password.length < 3) return toast.warning('Please enter longer credentials.');
+        const res = await request('GET', `/api/authentication/login?username=${username}&password=${password}`);
+        console.log(res);
         if (res.ok) {
-            toast.success('Login successful!')
-            window.location.reload()
+            toast.success('Login successful!');
+            window.location.reload();
         } else if (res.status === 429) {
-            toast.error('Too many requests. Please wait a moment and try again.')
+            toast.error('Too many requests. Please wait a moment and try again.');
         } else {
-            toast.error('Login failed. Please check your credentials.')
+            toast.error('Login failed. Please check your credentials.');
         }
-    }
+    };
 
     return (
         <main className="flex items-center justify-center min-h-screen bg-muted">
@@ -37,27 +45,25 @@ const SignInPage = () => {
                     </CardAction>
                 </CardHeader>
                 <CardContent>
-                    <form onSubmit={handleSubmit}>
-                        <div className="flex flex-col gap-6">
-                            <div className="grid gap-2">
-                                <Label htmlFor="username">Username</Label>
-                                <Input id="username" type="text" placeholder="john.doe" required />
-                            </div>
-                            <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Button
-                                        disabled
-                                        variant="link"
-                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </Button>
-                                </div>
-                                <Input id="password" type="password" required />
-                            </div>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="username">Username</Label>
+                            <Input id="username" type="text" placeholder="john.doe" required />
                         </div>
-                    </form>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <Button
+                                    disabled
+                                    variant="link"
+                                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                >
+                                    Forgot your password?
+                                </Button>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                    </div>
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
                     <Button type="submit" onClick={handleSubmit} className="w-full">
@@ -69,7 +75,7 @@ const SignInPage = () => {
                 </CardFooter>
             </Card>
         </main>
-    )
-}
+    );
+};
 
-export default SignInPage
+export default SignInPage;
