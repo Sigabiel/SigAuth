3929cce075e56bb6683f2798160dfc2a0b77f013
Ben Hacker <Sigabiel123@gmail.com>
2025-09-15 17:33:44 +0200

    declare prisma-wrapper als module

--- packages/prisma-wrapper/src/extended-prisma.ts
+++ packages/prisma-wrapper/src/extended-prisma.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "../prisma/generated/client";
+import { Prisma } from '../prisma/generated/client/client.js';
 
 export type AccountWithPermissions = Prisma.AccountGetPayload<{
     include: {
