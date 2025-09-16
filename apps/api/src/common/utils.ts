3525e71588a4200289eb973f758a0e0bc6235f11
LEONARDOX <42386676+LEONARD0X@users.noreply.github.com>
2025-09-16 17:35:16 +0200

    Use crypto for token generation

--- apps/api/src/common/utils.ts
+++ apps/api/src/common/utils.ts
@@ -1,14 +1,10 @@
 export const Utils = {
     convertPermissionNameToIdent: (name: string) => name.toLowerCase().replace(/ /g, '-'),
     generateToken: (length: number) => {
-        let result = '';
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        let counter = 0;
-        while (counter < length) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            counter += 1;
-        }
-        return result;
+        const array = new Uint8Array(length);
+        crypto.getRandomValues(array);
+        const generatedToken = Array.from(array, x => characters[x % characters.length]).join('');
+        return generatedToken;
     },
 };
